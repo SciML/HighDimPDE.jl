1d9725a892060e0e47e157406756f3f722c07a9b
bvictor <bvictor@ethz.ch>
2021-04-06 17:17:17 +0000

    added and renamed examples

--- NNPDEDS_tests_hamel.jl
+++ NNPDEDS_tests_hamel.jl
@@ -9,17 +9,17 @@ include("pde_solve_deepsplitting_4.jl")
 # using NeuralPDE
 
 using Random
-Random.seed!(100)
+# Random.seed!(100)
 
-d = 1 # number of dimensions
+d = 10 # number of dimensions
 # one-dimensional heat equation
 X0 = fill(0.0f0,d)  # initial points
 tspan = (0.0f0,1f0)
-dt = 0.005f0   # time step
-batch_size = 16000
+dt = 0.05f0   # time step
+batch_size = 8192
 train_steps = 16000
-σ_sampling = 0.7f0
-K = 1f0
+σ_sampling = 0.1f0
+K = 100f0
 
 hls = d + 50 #hidden layer size
 
@@ -33,12 +33,12 @@ opt = Flux.Optimiser(ExpDecay(0.1,
                 1e-4),
                 ADAM())
 
-g(X) = exp.(-0.5f0 * sum(X.^2,dims=1))   # terminal condition
-a(X) = 1f0 .- 0.1f0 .* sum(X.^2,dims=1)
+g(X) = Float32(2^(d/2)) * exp.(-Float32(2*π) * sum(X.^2,dims=1)) # terminal condition
+a(X) = - sum(X.^2,dims=1) / 2f0
 _0 = CUDA.zeros(Float32,1,batch_size)
-f(y,z,v_y,v_z,dv_y,dv_z,p,t) = max.(v_y , _0) .* (a(y) - 0.1f0 * max.(v_z , _0) .* (Float32(π^(d/2) .* σ_sampling^d) .* exp.( sum(z.^2,dims=1)) / σ_sampling^2)) # function from solved equation
+f(y,z,v_y,v_z,dv_y,dv_z,p,t) = max.(v_y , _0) .* (a(y) - max.(v_z , _0) .* a(z) .* (Float32(2*π^(d/2) .* σ_sampling^d) .* exp.(0.5f0 / σ_sampling^2  * sum(z.^2,dims=1)))) # function from solved equation
 μ(X,p,t) = 0f0
-σ(X,p,t) = 0.1f0 * sqrt(2f0)
+σ(X,p,t) = .1f0
 mc_sample(x) = CUDA.randn(d, batch_size) * σ_sampling
 
 ## One should look at InitialPDEProblem, this would surely be more appropriate
