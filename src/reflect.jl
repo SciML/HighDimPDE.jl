cbe8c83f3249974d651ac355203bc3482f5e8dd5
Victor <bvictor@ethz.ch>
2021-06-08 14:27:12 +0200

    working on _reflect_GPU2 . Not working yet. needs reflection to avoid division by (b-a).

--- src/reflect.jl
+++ src/reflect.jl
@@ -55,3 +55,107 @@ function _reflect(a,b,s,e)
     # Plots.plot3d!(dat[1,:],dat[2,:],dat[3,:],label = "",color="blue")
     return b
 end
+
+# """
+#     _reflect(a,b,s,e)
+# reflection of the vector (b-a) from a on the cube [s,e]^2
+# """
+# function _reflect_GPU(a,b,s,e,batch_size,_device)
+#     n = zeros(size(a)) |> _device 
+#     # s = fill(s,batch_size) |> _device
+#     # e = fill(e,batch_size) |> _device 
+#     # first checking if b is in the hypercube
+#     # if it is not, then r becomes less than one
+#     r = fill(2.,batch_size) |> _device
+#     rtemp = similar(r)
+#     rtemparg = zeros(batch_size) |> _device 
+#     #TODO: change "for i in 1:length(a)" to "for i in 1:size(a,1)"
+#     # right now the scheme is not efficient, as it proceeds one reflection for one batch at a time
+#     for i in 1:size(a,1)
+#         bi = @view b[i,:]
+#         ai = @view a[i,:]
+#         ni = @view n[i,:]
+#         if prod(bi .< s)
+#             @. rtemp = (ai - s) / (ai - bi)
+#             if prod(rtemp .< r)
+#                 r = rtemp
+#                 n .= 0
+#                 n[i] = -1
+#             end
+#         elseif  prod(bi .> e)
+#             rtemp =  (e - a[i]) / (b[i]- a[i])
+#             if rtemp < r
+#                 r = rtemp
+#                 n .= 0
+#                 n[i] = 1
+#             end
+#         end
+#     end
+#     while r < 1
+#         c = a + r * ( b - a )
+#         # dat = hcat(a,c)
+#         # Plots.plot3d!(dat[1,:],dat[2,:],dat[3,:],label = "",color="blue")
+#         a = c
+#         b = b - 2 * n * ( dot(b-c,n))
+#         r = 2;
+#         for i in 1:length(a)
+#             if b[i] < s
+#                 rtemp = (a[i] - s) / (a[i] - b[i])
+#                 if rtemp < r
+#                     r = rtemp
+#                     n .= 0
+#                     n[i] = -1
+#                 end
+#             elseif  b[i] > e
+#                 rtemp =  (e - a[i]) / (b[i]- a[i])
+#                 if rtemp < r
+#                     r = rtemp
+#                     n .= 0
+#                     n[i] = 1
+#                 end
+#             end
+#         end
+#     end
+#     # dat = hcat(a,b)
+#     # Plots.plot3d!(dat[1,:],dat[2,:],dat[3,:],label = "",color="blue")
+#     return b
+# end
+
+using SparseArrays
+"""
+    _reflect(a,b,s,e)
+reflection of the vector (b-a) from a on the cube [s,e]^2
+"""
+function _reflect_GPU2(a,b,s,e,d,batch_size)
+    out1 = b .< s
+    out2 = b .> e
+    rtemp1 = @. (a - s) #left 
+    rtemp2 = @. (e - a) #right
+    rtemp = fill(2.,size(a))
+    rtemp[out1] .= rtemp1[out1]
+    rtemp[out2] .= rtemp2[out2]
+    imin = argmin.(eachcol(rtemp))
+    rmin = minimum(rtemp,dims=1)
+    rmin[rmin .> 1.] .= 0.
+    n = sparse(imin,1:batch_size,1,d,batch_size)
+
+    while sum(rmin) .> 0.
+        c = @. (a + ( * rmin) * n)
+        b = @.( b - 2 * n * (c-b) )
+        a = c
+
+        out1 = b .< s
+        out2 = b .> e
+        rtemp1 = @. (a - s) / (a - b) #left 
+        rtemp2 = @. (e - a) / (b - a) #right
+        rtemp = fill(2.,size(a))
+        rtemp[out1] .= rtemp1[out1]
+        rtemp[out2] .= rtemp2[out2]
+        imin = argmin.(eachcol(rtemp))
+        rmin = minimum(rtemp,dims=1)
+        rmin[rmin .> 1.] .= 0.
+        n = sparse(imin,1:batch_size,1,d,batch_size)
+    end
+    return b
+end
+
--- test/reflect.jl
+++ test/reflect.jl
@@ -1,13 +1,17 @@
 ### testing reflect method
 using Test
 # testing reflection on batchsize 0
-X0 = fill(0.0f0,10)  
+d = 10
+X0 = fill(0.0f0,d)  
 X1 = X0 + randn(d)
 X11 = _reflect(X0,X1,0,1)
 @test prod(X11 .< 1) && prod(X11 .> 0) 
 
 # testing reflection on batchsize 
+batch_size = 1000
 y0 = repeat(X0[:],1,batch_size)
 y1 = y0 + randn(size(y0))
-y11 = _reflect(y0,y1,0,1)
+# y0 = hcat(X0,y0)
+# y1 = hcat(X1,y1)
+y11 = _reflect_GPU2(y0,y1,0,1,d,batch_size)
 @test prod(y11 .< 1) && prod(y11 .> 0) 
