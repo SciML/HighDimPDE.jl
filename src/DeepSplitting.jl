80e41174772eed963ab19291c7bb429eda84ff7c
Victor <bvictor@ethz.ch>
2021-07-14 10:42:12 +0200

    changed params update

--- src/DeepSplitting.jl
+++ src/DeepSplitting.jl
@@ -151,7 +151,7 @@ function solve(
 
         # saving
         vi = deepcopy(vj)
-        vj = deepcopy(nn)
+        # vj = deepcopy(nn)
         ps = Flux.params(vj)
         if isnothing(u_domain)
             push!(usol, mean(vi(x0)) |> cpu)
