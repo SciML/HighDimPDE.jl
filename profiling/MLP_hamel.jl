cc1d20600b6333accea111b7e74b03bde953cd58
bvictor <bvictor@ethz.ch>
2021-09-30 07:43:28 +0000

    updated profiling

--- profiling/MLP_hamel.jl
+++ profiling/MLP_hamel.jl
@@ -8,7 +8,7 @@ using PyPlot
 using UnPack
 plotting = true
 
-d = 2
+d = 10
 x = fill(0.,d)  # initial point
 
 tspan = (0e0, 1e0)
@@ -18,7 +18,7 @@ tspan = (0e0, 1e0)
 ss0 = 1 / 4 / π #std g0
 
 σ_sampling = 1.0
-alg = MLP(M=4, K=10, L = 4, mc_sample = NormalSampling(σ_sampling) )
+alg = MLP(M=5, K=10, L = 5, mc_sample = NormalSampling(σ_sampling) )
 
 
 ##########################
@@ -26,11 +26,11 @@ alg = MLP(M=4, K=10, L = 4, mc_sample = NormalSampling(σ_sampling) )
 ##########################
 g(x) = (2*π)^(-d/2) * ss0^(- Float64(d) * 5e-1) * exp.(-5e-1 *sum(x.^2)/ ss0) # initial condition
 m(x) = - 5e-1 * sum(x.^2)
-f(y, z, v_y, v_z, ∇v_y, ∇v_z, t) = max(0.0, v_y) * (m(y) - max(0.0, v_z) * m(z) * (2.0 * π)^(d/2) * σ_sampling^d * exp(0.5 * sum(z.^2) / σ_sampling^2)) # nonlocal nonlinear part of the
+f(y, z, v_y, v_z, ∇v_y, ∇v_z, p, t) = max(0.0, v_y) * (m(y) - max(0.0, v_z) * m(z) * (2.0 * π)^(d/2) * σ_sampling^d * exp(0.5 * sum(z.^2) / σ_sampling^2)) # nonlocal nonlinear part of the
 
 # defining the problem
 prob = PIDEProblem(g, f, μ, σ, tspan, x = x)
 
 # solving
-@time xs,ts,sol = solve(prob, alg, verbose = false, multithreading=false)
+@time xs,ts,sol = solve(prob, alg, verbose = false, multithreading=true)
 println("g(x) = " , g(x), "\nResult = ", sol[end])
