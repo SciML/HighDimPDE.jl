0309f75d16aaefd17096dae1a62dac8b9d88eb25
Victor <bvictor@ethz.ch>
2022-06-23 11:39:48 +0200

    fixed docs for integratin in SciMLDocs

--- docs/make.jl
+++ docs/make.jl
@@ -1,15 +1,10 @@
 using Documenter, HighDimPDE
 
+include("pages.jl")
+
 makedocs(sitename="HighDimPDE.jl",
         format = Documenter.HTML(prettyurls = false),
         authors = "Victor Boussange",
-        pages = [
-            "Home" => "index.md",
-            "Getting started" => "getting_started.md",
-            "Solver Algorithms" => ["MLP.md", 
-                        "DeepSplitting.md", 
-                        ],
-            "Feynman Kac formula" => "Feynman_Kac.md",
-        ],)
+        pages = pages)
 
 deploydocs(repo = "github.com/SciML/HighDimPDE.jl", devbranch="main")
---
+++ docs/pages.jl
@@ -0,0 +1,8 @@
+pages = [
+            "Home" => "index.md",
+            "Getting started" => "getting_started.md",
+            "Solver Algorithms" => ["MLP.md", 
+                        "DeepSplitting.md", 
+                        ],
+            "Feynman Kac formula" => "Feynman_Kac.md",
+        ]
