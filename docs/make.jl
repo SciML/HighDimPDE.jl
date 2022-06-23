43cd49b717c94f30caf45d0aa2957b645de174cf
Victor <bvictor@ethz.ch>
2022-06-23 11:45:22 +0200

    added strict code

--- docs/make.jl
+++ docs/make.jl
@@ -5,6 +5,15 @@ include("pages.jl")
 makedocs(sitename="HighDimPDE.jl",
         format = Documenter.HTML(prettyurls = false),
         authors = "Victor Boussange",
-        pages = pages)
+        pages = pages,
+        strict=[
+        :doctest, 
+        :linkcheck, 
+        :parse_error,
+        :example_block,
+        # Other available options are
+        # :autodocs_block, :cross_references, :docs_block, :eval_block, :example_block, :footnote, :meta_block, :missing_docs, :setup_block
+        ],
+        )
 
 deploydocs(repo = "github.com/SciML/HighDimPDE.jl", devbranch="main")
