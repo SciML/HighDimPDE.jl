9f19a7a10009bd4515f23a73ee2d7aee8d013486
Arno Strouwen <arno.strouwen@telenet.be>
2023-09-25 04:39:55 +0200

    Documenter 1.0 upgrade

--- docs/make.jl
+++ docs/make.jl
@@ -8,14 +8,7 @@ include("pages.jl")
 makedocs(sitename="HighDimPDE.jl",
         authors = "Victor Boussange",
         pages = pages,
-        strict=[
-        :doctest,
-        :linkcheck,
-        :parse_error,
-        :example_block,
-        # Other available options are
-        # :autodocs_block, :cross_references, :docs_block, :eval_block, :example_block, :footnote, :meta_block, :missing_docs, :setup_block
-        ],
+        clean = true, doctest = false, linkcheck = true,
         format = Documenter.HTML(assets = ["assets/favicon.ico"],
                                  canonical = "https://docs.sciml.ai/HighDimPDE/stable/"),)
 
