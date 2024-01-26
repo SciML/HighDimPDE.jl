3faa879c8ee1f4bcfb8204ab7463788ca45a474c
ashutosh-b-b <a.bharambe123@gmail.com>
2024-01-27 00:34:31 +0530

    test: add tests for NNStopping
    - adds test for a Bermudan max call example from paper
    - adds test for a Put Basket in Duprie's local volatility model

--- test/NNStopping.jl
+++ test/NNStopping.jl
@@ -1,49 +1,68 @@
-using Test, Flux , StochasticDiffEq , LinearAlgebra
+using Test, Flux, StochasticDiffEq, LinearAlgebra
 println("Optimal Stopping Time Test")
-using NeuralPDE
+using HighDimPDE
 
 using Random
-Random.seed!(100)
-
-d = 1
-r = 0.04f0
-beta = 0.2f0
-T = 1
-u0 = fill(80.00 , d , 1)
-f(du,u,p,t) = (du .= r*u)
-sigma(du,u,p,t)  = (du .= Diagonal(beta*u))
-tspan = (0.0 , 1.0)
-N = 50
-dt = tspan[2]/49
+Random.seed!(10)
+# Bermudan Max-Call Standard Example
+d = 3
+r = 0.05
+beta = 0.2
+T = 3
+u0 = fill(90.0, d)
+delta = 0.1
+f(du, u, p, t) = du .= (r - delta) * u
+sigma(du, u, p, t) = du .= beta * u
+tspan = (0.0, T)
+N = 9
+dt = T / (N)
 K = 100.00
-function g(t , x)
-  return exp(-r*t)*(max(K -  maximum(x)  , 0))
+function g(t, x)
+    return exp(-r * t) * (max(maximum(x) - K, 0))
 end
 
-prob  = SDEProblem(f , sigma , u0 , tspan ; g = g)
-opt = Flux.ADAM(0.1)
-m = Chain(Dense(d , 5, tanh), Dense(5, 16 , tanh)  , Dense(16 , N ), softmax)
-sol = solve(prob, NeuralPDE.NNStopping(m, opt), verbose = true, dt = dt, sde_alg = EM()
-            abstol=1e-6, maxiters = 20 , trajectories = 200)
+prob = SDEProblem(f, sigma, u0, tspan; payoff = g)
+models = [Chain(Dense(d + 1, 32, tanh), BatchNorm(32, tanh), Dense(32, 1, sigmoid))
+          for i in 1:N]
+opt = Flux.Optimisers.Adam(0.01)
+alg = NNStopping(models, opt)
+sol = solve(prob, alg, SRIW1(); dt = dt, trajectories = 1000, maxiters = 1000, verbose = true)
+analytical_sol = 11.278 # Ref [1]
+@test abs(analytical_sol - sol.payoff) < 0.5
 
-##Analytical Binomial Tree approach for American Options
-function BinomialTreeAM1D(S0 , N , r , beta)
-    V = zeros(N+1)
-    dT = T/N
-    u = exp(beta*sqrt(dT))
-    d = 1/u
-    S_T = [S0*(u^j)* (d^(N-j)) for j in 0:N]
-    a = exp(r*dT)
-    p = (a - d)/(u - d)
-    q = 1.0 - p
-    V = [max(K - x , 0) for x in S_T]
-    for i in N-1:-1:0
-      V[1:end-1] = exp(-r*dT).*(p*V[2:end] + q*V[1:end-1])
-      S_T = S_T*u
-      V = [max(K - S_T[i] , V[i]) for i in 1:size(S_T)[1]]
-    end
-    return V[1]
+# Put basket option in Dupire’s local volatility model
+d = 5
+r = 0.05
+beta = 0.2
+T = 1.0
+u0 = fill(100.0, d)
+delta = 0.1
+f(u, p, t) = (r - delta) * u
+
+function B(t, x)
+    x * 0.6 * exp(-0.05 * sqrt(t)) *
+    (1.2 - exp(-0.1 * t - 0.001 * (exp(-r * t * x - first(u0))^2)))
 end
-real_sol = BinomialTreeAM1D(u0[1] , N , r , beta)
-error = abs(sol - real_sol)
-@test error < 0.5
+sigma(u, p, t) = B.(Ref(t), u)
+
+tspan = (0.0f0, T)
+N = 10
+dt = T / (N)
+K = 100.00
+function g(t, x)
+    return exp(-r * t) * (max(K - mean(x), 0))
+end
+prob = SDEProblem(f, sigma, u0, tspan; payoff = g)
+models = [Chain(Dense(d + 1, 32, tanh), BatchNorm(32, tanh), Dense(32, 1, sigmoid))
+          for i in 1:N]
+opt = Flux.Optimisers.Adam(0.01)
+alg = NNStopping(models, opt)
+sol = solve(prob, alg, SRIW1(); dt = dt, trajectories = 1000, maxiters = 500, verbose = true)
+analytical_sol = 6.301 # Ref [2]
+@test abs(analytical_sol - sol.payoff) < 0.5
+
+### References for Analytical Payoffs:
+#=
+[1] Section 4.4.1: Solving high-dimensional optimal stopping problems using deep learning (https://arxiv.org/pdf/1908.01602.pdf) 
+[2] Section 4.4.3: Solving high-dimensional optimal stopping problems using deep learning (https://arxiv.org/pdf/1908.01602.pdf) 
+=#
