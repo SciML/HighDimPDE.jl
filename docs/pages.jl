de266d26818e634b3a82ca0e3b4719dd2b3e7ded
ashutosh-b-b <a.bharambe123@gmail.com>
2024-01-30 13:23:40 +0530

    docs: add tutorial and page to pages.jl

--- docs/pages.jl
+++ docs/pages.jl
@@ -3,11 +3,13 @@ pages = [
     "Getting started" => "getting_started.md",
     "Solver Algorithms" => ["MLP.md",
         "DeepSplitting.md",
-        "DeepBSDE.md"],
+        "DeepBSDE.md", 
+        "NNStopping.md"],
     "Tutorials" => [
         "tutorials/deepsplitting.md",
         "tutorials/deepbsde.md",
         "tutorials/mlp.md",
+        "tutorials/nnstopping.md"
     ],
     "Feynman Kac formula" => "Feynman_Kac.md",
 ]
