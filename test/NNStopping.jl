31a64f178681579334b05aee00d3e16d6d6e35c6
ashutosh-b-b <a.bharambe123@gmail.com>
2024-01-31 01:58:44 +0530

    test: update tests

--- test/NNStopping.jl
+++ test/NNStopping.jl
@@ -18,7 +18,7 @@ tspan = (0.0, T)
 N = 9
 dt = T / (N)
 K = 100.00
-function g(t, x)
+function g(x, t)
     return exp(-r * t) * (max(maximum(x) - K, 0))
 end
 
@@ -56,10 +56,10 @@ tspan = (0.0f0, T)
 N = 10
 dt = T / (N)
 K = 100.00
-function g(t, x)
+function g(x, t)
     return exp(-r * t) * (max(K - mean(x), 0))
 end
-prob = SDEProblem(f, sigma, u0, tspan; payoff = g)
+prob = PIDEProblem(f, sigma, u0, tspan; payoff = g)
 models = [Chain(Dense(d + 1, 32, tanh), BatchNorm(32, tanh), Dense(32, 1, sigmoid))
           for i in 1:N]
 opt = Flux.Optimisers.Adam(0.01)
